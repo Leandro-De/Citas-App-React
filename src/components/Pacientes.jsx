b3e5f88833e33270bb613c483a942d6229ebc227
leandro <yleandrovarela@gmail.com>
2023-04-24 08:21:15 -0500

    add pacientes por componente

---
+++ src/components/Pacientes.jsx
@@ -0,0 +1,28 @@
+
+const Pacientes = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+          <span className="font-normal normal-case">Hook</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
+          <span className="font-normal normal-case">Hook</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">E-mail: {''}
+          <span className="font-normal normal-case">Hook</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {''}
+          <span className="font-normal normal-case">Hook</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: {''}
+          <span className="font-normal normal-case">Hook</span>
+        </p>
+      </div>
+  )
+}
+
+export default Pacientes
